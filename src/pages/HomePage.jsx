b343b1ee85be3909fe64e586290b452cd25a4ece
AaronNgoi <7630063-AaronNgoi@users.noreply.replit.com>
2023-05-03 01:50:46 +0000

    Fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Header from '../Header';
 import ShrunkHabitTracker from '../ShrunkHabitTracker';
 import { useHabits } from '../context/HabitContext';
